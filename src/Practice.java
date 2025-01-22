08b4de0a7dcbf1384ce2fac4d5c234ef067510cd
PalleBaby <131858465+PalleBaby@users.noreply.github.com>
2025-01-22 18:16:24 +0530

    Update Practice.java

--- src/Practice.java
+++ src/Practice.java
@@ -4,6 +4,7 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git remote!!");
+		System.out.println("Day1 completed");
 	}
 
 }
